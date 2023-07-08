9aaa2a431efc86ccdab88dca8a2ffa8debec46fb
Mariana Lima <mariana.lima118@outlook.com>
2023-07-07 23:36:54 -0300

    0107 ex04

---
+++ fundamentos/exe0107/ex04.php
@@ -0,0 +1,70 @@
+<?php
+
+$x = 0;
+$count = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$x = $_POST["value"];
+
+	for($i = 0; $i <= $x; $i++){
+		array_push($count, $i);
+	}
+}
+
+function countNumber($item){
+	foreach($item as $num){
+		echo $num ."\n";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<title>Document</title>
+</head>
+
+<body>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+
+		body {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 2rem;
+		}
+
+		nav {
+			width: 100%;
+		}
+
+		form {
+			width: 100%;
+			max-width: 500px;
+			margin-top: 2rem;
+		}
+	</style>
+
+	<form method="POST">
+		<div class="mb-3">
+			<label for="value" class="form-label">Informe um numero inteiro:</label>
+			<input type="text" id="" name="value" class="form-control" placeholder="ex: 10.99">
+		</div>
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+	<ul class="list-group">
+        <li class="list-group-item d-inline-flex row-gap-3">Contagem: <?php echo countNumber($count)?></li>
+    </ul>
+</body>
+
+</html>
