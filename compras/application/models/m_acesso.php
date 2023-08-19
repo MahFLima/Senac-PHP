49ec89346cfbe5ac36a802c512a9a8130277fb64
Mariana Lima <mariana.lima118@outlook.com>
2023-08-19 13:51:44 -0300

    compras-CI3 first commit

---
+++ compras/application/models/m_acesso.php
@@ -0,0 +1,17 @@
+<?php
+    defined('BASEPATH') or exit('No direct script access allowed');
+
+    class m_acesso extends CI_Model{
+        public function validalogin($usuario, $senha){
+            $retorno = $this->db->query("select * from usuarios where usuario = '$usuario' and senha = md5('$senha') and state = ''");
+
+            if($retorno->num_rows() > 0){
+                $dados = array('codigo' => 1, 'msg' => 'Usuario Correto');
+            } else {
+                $dados = array('codigo' => 4, 'msg' => 'Usuario não encontrado');
+            }
+
+            return $dados;
+        }
+    }
+?>
---
+++ compras/application/models/m_usuario.php
@@ -0,0 +1,7 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class m_usuario extends CI_Model{
+
+    } 
+?>
